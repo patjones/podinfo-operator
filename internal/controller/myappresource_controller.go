596d14fe0af3330cccab6bb02899c70bc249a7cd
Patrick Jones <patrick@caffeine.tv>
2024-03-14 15:48:56 -0400

    no longer sidecaring redis, removed troublesome crtl returns

--- internal/controller/myappresource_controller.go
+++ internal/controller/myappresource_controller.go
@@ -46,8 +46,6 @@ func createDeployment(myappresource myapigroupv1alpha1.MyAppResource, r MyAppRes
 		"app.kubernetes.io/managed-by": "podinfo-operator",
 	}
 
-	// ...
-
 	environmentVars := []corev1.EnvVar{
 		{
 			Name:  "PODINFO_UI_COLOR",
@@ -61,7 +59,7 @@ func createDeployment(myappresource myapigroupv1alpha1.MyAppResource, r MyAppRes
 	if myappresource.Spec.Redis.Enabled {
 		environmentVars = append(environmentVars, corev1.EnvVar{
 			Name:  "PODINFO_CACHE_SERVER",
-			Value: "tcp://localhost:6379",
+			Value: "tcp://" + myappresource.Name + "-redis:6379",
 		})
 	}
 
@@ -100,10 +98,6 @@ func createDeployment(myappresource myapigroupv1alpha1.MyAppResource, r MyAppRes
 								},
 							},
 							Env: environmentVars,
-						},
-						{
-							Name:  "redis",
-							Image: "redis:alpine",
 						},
 					},
 				},
@@ -144,6 +138,141 @@ func createService(myappresource myapigroupv1alpha1.MyAppResource, r MyAppResour
 	return svc
 }
 
+func createRedisDeployment(myappresource myapigroupv1alpha1.MyAppResource, r MyAppResourceReconciler) *appsv1.Deployment {
+
+	labels := map[string]string{
+		"app":                          myappresource.Name + "-redis",
+		"app.kubernetes.io/name":       myappresource.Name + "-redis",
+		"app.kubernetes.io/managed-by": "podinfo-operator",
+	}
+
+	replicas := int32(1)
+	dep := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      myappresource.Name + "-redis",
+			Namespace: myappresource.Namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &replicas,
+
+			Selector: &metav1.LabelSelector{
+				MatchLabels: labels,
+			},
+
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{Labels: labels},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "redis",
+							Image: "redis:5.0.4",
+							Ports: []corev1.ContainerPort{
+								{
+									Name:          "redis",
+									ContainerPort: 6379,
+									Protocol:      corev1.ProtocolTCP,
+								},
+							},
+							ReadinessProbe: &corev1.Probe{
+								ProbeHandler: corev1.ProbeHandler{
+									Exec: &corev1.ExecAction{
+										Command: []string{"redis-cli", "ping"},
+									},
+								},
+							},
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									Name:      "redis-data",
+									MountPath: "/var/lib/redis",
+								},
+								{
+									Name:      "redis-config",
+									MountPath: "redis-master",
+								},
+							},
+						},
+					},
+					Volumes: []corev1.Volume{
+						{
+							Name: "redis-data",
+							VolumeSource: corev1.VolumeSource{
+								EmptyDir: &corev1.EmptyDirVolumeSource{},
+							},
+						},
+						{
+							Name: "redis-config",
+							VolumeSource: corev1.VolumeSource{
+								ConfigMap: &corev1.ConfigMapVolumeSource{
+									LocalObjectReference: corev1.LocalObjectReference{
+										Name: myappresource.Name + "-redis",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	ctrl.SetControllerReference(&myappresource, dep, r.Scheme)
+	return dep
+}
+
+func createRedisService(myappresource myapigroupv1alpha1.MyAppResource, r MyAppResourceReconciler) *corev1.Service {
+
+	labels := map[string]string{
+		"app":                          myappresource.Name + "-redis",
+		"app.kubernetes.io/name":       myappresource.Name + "-redis",
+		"app.kubernetes.io/managed-by": "podinfo-operator",
+	}
+
+	svc := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      myappresource.Name + "-redis",
+			Namespace: myappresource.Namespace,
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: labels,
+			Ports: []corev1.ServicePort{
+				{
+					Name:       "redis",
+					Protocol:   corev1.ProtocolTCP,
+					Port:       6379,
+					TargetPort: intstr.FromInt(6379),
+				},
+			},
+		},
+	}
+	ctrl.SetControllerReference(&myappresource, svc, r.Scheme)
+	return svc
+}
+
+func createRedisConfigMap(myappresource myapigroupv1alpha1.MyAppResource, r MyAppResourceReconciler) *corev1.ConfigMap {
+	labels := map[string]string{
+		"app":                          myappresource.Name + "-redis",
+		"app.kubernetes.io/name":       myappresource.Name + "-redis",
+		"app.kubernetes.io/managed-by": "podinfo-operator",
+	}
+
+	cm := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      myappresource.Name + "-redis",
+			Namespace: myappresource.Namespace,
+			Labels:    labels,
+		},
+		Data: map[string]string{
+			"redis.conf": `
+				maxmemory 64mb
+				maxmemory-policy allkeys-lru
+				save ""
+				appendonly no
+			`,
+		},
+	}
+	ctrl.SetControllerReference(&myappresource, cm, r.Scheme)
+	return cm
+}
+
 //+kubebuilder:rbac:groups=my.api.group,resources=myappresources,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=my.api.group,resources=myappresources/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=my.api.group,resources=myappresources/finalizers,verbs=update
@@ -177,16 +306,11 @@ func (r *MyAppResourceReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 			deployment = *createDeployment(myappresource, *r)
 			err = r.Create(ctx, &deployment)
 			if err != nil {
-
 				log.Log.Error(err, "Error creating Deployment for MyAppResource", "deployment", deployment)
-				return ctrl.Result{}, err
 			}
-			return ctrl.Result{}, nil
+			log.Log.Info("Deployment created successfully", "deployment", deployment)
 		}
-		log.Log.Error(err, "unable to fetch Deployment for MyAppResource", "deployment", deployment)
-		return ctrl.Result{}, err
 	}
-
 	//Manage Service
 	service := corev1.Service{}
 	err = r.Get(ctx, client.ObjectKey{Namespace: myappresource.ObjectMeta.Namespace, Name: myappresource.ObjectMeta.Name}, &service)
@@ -197,12 +321,50 @@ func (r *MyAppResourceReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 			err = r.Create(ctx, &service)
 			if err != nil {
 				log.Log.Error(err, "Error creating Service for MyAppResource", "service", service)
-				return ctrl.Result{}, err
 			}
-			return ctrl.Result{}, nil
 		}
-		log.Log.Error(err, "unable to fetch Service for MyAppResource", "service", service)
-		return ctrl.Result{}, err
+	}
+
+	//Manage Redis Deployment
+	redisDeployment := appsv1.Deployment{}
+	err = r.Get(ctx, client.ObjectKey{Namespace: myappresource.ObjectMeta.Namespace, Name: myappresource.ObjectMeta.Name + "-redis"}, &redisDeployment)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			log.Log.Info("Creating Redis Deployment", "Namespace", myappresource.ObjectMeta.Namespace, "Name", myappresource.ObjectMeta.Name+"-redis")
+			redisDeployment = *createRedisDeployment(myappresource, *r)
+			err = r.Create(ctx, &redisDeployment)
+			if err != nil {
+				log.Log.Error(err, "Error creating Deployment for Redis", "deployment", redisDeployment)
+			}
+		}
+	}
+
+	//Manage Redis Service
+	redisService := corev1.Service{}
+	err = r.Get(ctx, client.ObjectKey{Namespace: myappresource.ObjectMeta.Namespace, Name: myappresource.ObjectMeta.Name + "-redis"}, &redisService)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			log.Log.Info("Creating Redis Service", "Namespace", myappresource.ObjectMeta.Namespace, "Name", myappresource.ObjectMeta.Name+"-redis")
+			redisService = *createRedisService(myappresource, *r)
+			err = r.Create(ctx, &redisService)
+			if err != nil {
+				log.Log.Error(err, "Error creating Service for Redis", "service", redisService)
+			}
+		}
+	}
+
+	//Manage Redis ConfigMap
+	redisConfigMap := corev1.ConfigMap{}
+	err = r.Get(ctx, client.ObjectKey{Namespace: myappresource.ObjectMeta.Namespace, Name: myappresource.ObjectMeta.Name + "-redis"}, &redisConfigMap)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			log.Log.Info("Creating Redis ConfigMap", "Namespace", myappresource.ObjectMeta.Namespace, "Name", myappresource.ObjectMeta.Name+"-redis")
+			redisConfigMap = *createRedisConfigMap(myappresource, *r)
+			err = r.Create(ctx, &redisConfigMap)
+			if err != nil {
+				log.Log.Error(err, "Error creating ConfigMap for Redis", "configmap", redisConfigMap)
+			}
+		}
 	}
 
 	return ctrl.Result{}, nil
